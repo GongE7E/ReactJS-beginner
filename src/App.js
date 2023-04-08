26c00e176edf1138a73a4ce2f6c4693f6c2b301a
gong <gong4737@icloud.com>
2023-04-08 10:55:51 +0900

    clean-up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import { useState,useEffect } from "react";
 import Button from "./Button"
 
+function Hello(){
+  useEffect(()=>{
+    console.log("hello :)");
+    return()=>console.log("bye :(");
+  },[]);
+  return(
+    <h1>HEllo!</h1>
+  );
+}
+
 function App() {
-  const [counter,setValue]=useState(0);
-  const [keyword,setKeyword]=useState("");
-  const onChange=(event)=>setKeyword(event.target.value);
-  const onClick=()=>setValue((current)=>current+1);
-  console.log("I run all the time ");
-  useEffect(()=>{console.log("CALL THE API...")},[]);
-  useEffect(()=>{
-    if(keyword !="" && keyword.length>5){
-    console.log("SEARCH FOR",keyword);
-  }
-  },[keyword]);
-  useEffect(()=>{
-    console.log("i run when keyword &counter changes")},
-    [keyword, counter]);
+  const [showing,setShowing]=useState(false);
+  const onClick=()=>setShowing(prev=>!prev);
   return (
-    <div><h1>{counter}</h1>
-    <input value={keyword}onChange={onChange} type="text" placeholder="Search here..."/>
-    <button onClick={onClick}>Click Me</button>
+    <div>
+      {showing? <Hello/> : null}
+      <button onClick={onClick}>{showing? "Hide" :"Show"}</button>
     </div>
   );
 }
