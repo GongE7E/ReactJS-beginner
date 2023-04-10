4f75b6d0db834e2a4124eac7a923e7f0050d0812
gong <gong4737@icloud.com>
2023-04-10 19:12:39 +0900

    coin Tracker

--- src/App.js
+++ src/App.js
@@ -3,37 +3,21 @@ import Button from "./Button"
 
 
 function App() {
-  const [toDo,setToDo]=useState("");
-  const [toDos,setToDos]=useState([]);
-  const onChange=(event)=>setToDo(event.target.value);
-  const onSubmit=(event)=>{
-    event.preventDefault();
-    if(toDo===""){
-      return;
-    }
-    setToDos((currentArray)=>[toDo,...currentArray]);
-    setToDo("");
-  };
-  const onClick=()=>{
-    if(toDos===[]){
-      return;
-    }
-    setToDos([]);
-  }
-  return <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange}value={toDo}type="text" placeholder="Write your to do..."/>
-        <button >Add To Do</button>
-        <button onClick={onClick}>Delete</button>
-      </form>
-      <hr/>
-      <ul>
-     {toDos.map((item,index)=>(
-     <li key={index}>{item}</li>
-     ))}
-      </ul>
-    </div>
+const[loading,setLoading]=useState(true);
+const [coins,setCoins]=useState([]);
+useEffect(()=>{
+fetch("https://api.coinpaprika.com/v1/tickers").then((response)=>response.json())
+.then((json)=>{
+  setCoins(json);
+  setLoading(false);
+});
+},[]);
+  return<div>
+    <h1>The Coins!({coins.length})</h1>
+    {loading?<strong>Loading...</strong>:<select>
+      {coins.map((coin)=><option key={coin.id}>{coin.name}({coin.symbol}) :{coin.quotes.USD.price}</option>)}
+    </select>}
+  </div>
 }
 
 export default App;
